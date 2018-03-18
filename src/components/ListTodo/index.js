967ea63a637fe9f935cef8232c531441cdf77692
dwicao <lutfiandwica@gmail.com>
2018-03-19 06:58:57 +0700

    use prop-types

--- src/components/ListTodo/index.js
+++ src/components/ListTodo/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import { Actions, ActionConst } from 'react-native-router-flux';
 import {
   ScrollView,
